01ae87616a0d4fd3b1a98261cad45eedbf8e0720
wasm-forge <122647775+wasm-forge@users.noreply.github.com>
2025-03-21 21:41:12 +0100

    small fixes for provider

--- src/generator.rs
+++ src/generator.rs
@@ -1,41 +1,20 @@
-use std::{
-    env, fs,
-    path::{Path, PathBuf},
-};
+use std::{env, fs, path::PathBuf};
 
 use anyhow::Error;
-use ic_cdk_bindgen::code_generator::{self, CustomProvider};
+use ic_cdk_bindgen::code_generator;
 
 use crate::{
     arguments::IcTestArgs,
-    dfx_json::{CanisterSetup, parse_dfx_json},
+    dfx_json::{parse_dfx_json, CanisterSetup},
 };
 
 use askama::Template;
-
-pub async fn get_textual_content(path_or_url: &str) -> Result<String, Error> {
-    let local_path = Path::new(path_or_url);
-
-    if local_path.exists() && local_path.is_file() {
-        // Read from local file
-        let contents = fs::read_to_string(local_path)?;
-        Ok(contents)
-    } else if path_or_url.starts_with("http") {
-        // assume it's a URL and fetch via HTTP(S)
-        let response = reqwest::get(path_or_url).await?.text().await?;
-        Ok(response)
-    } else {
-        panic!("Failed to read from {path_or_url}");
-    }
-}
 
 #[derive(Template)]
 #[template(path = "mod.rs.txt")]
 struct ModRsTemplate<'a> {
     canisters: &'a Vec<CanisterSetup>,
 }
-
-struct MyProvider;
 
 pub async fn generate(args: &IcTestArgs) -> Result<(), Error> {
     let canisters = parse_dfx_json(args)?;
@@ -71,17 +50,21 @@ pub async fn generate(args: &IcTestArgs) -> Result<(), Error> {
             canister_file.push(format!("{}.rs", &canister.canister_name));
 
             // try parse candid file
-            let conf = code_generator::Config::new();
+            let mut config = code_generator::Config::new();
 
-            // set target to some provider
-            //conf.set_target(code_generator::Target::CustomProvider(MyProvider {}));
+            config.set_target(code_generator::Target::Provider);
+            config.set_service_name(format!("{}Canister", canister.canister_name));
 
             let (env, actor) = candid_parser::typing::check_str(&candid_content, true).unwrap();
 
-            let content = ic_cdk_bindgen::code_generator::compile(&conf, &env, &actor);
+            let content = ic_cdk_bindgen::code_generator::compile(&config, &env, &actor);
 
             fs::write(&canister_file, content)
                 .unwrap_or_else(|_| panic!("Could not write to file: {}", &canister.canister_name));
+            let output = std::process::Command::new("rustfmt")
+                .arg(&canister_file)
+                .output();
+            println!("{:?}", output);
         }
     }
 
